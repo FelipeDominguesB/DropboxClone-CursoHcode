d726b5e8e89fe10447dde7260c089bff107fc3d5
FelipeDominguesB <felipedom2001@hotmail.com>
2021-03-01 16:30:06 -0300

    Abre arquivos com double click agora

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,31 @@ router.get('/', function(req, res, next) {
 });
 
 
+router.get('/file', (req, res)=>{
+  let path = './' + req.query.path;
+
+  if(fs.existsSync(path)){
+    fs.readFile(path, (err, data)=>{
+      if(err)
+      {
+        console.log(err);
+        res.status(404).json({
+          error: err
+        });
+
+      }
+      else{
+        res.status(200).end(data)
+      }
+    });
+  }
+  else{
+    res.status(404).json({
+      error: 'File not found;'
+    })
+  }
+});
+
 router.delete('/file', (req, res)=>{
   let form = new formidable.IncomingForm({
     uploadDir: './upload',
@@ -31,6 +56,10 @@ router.delete('/file', (req, res)=>{
             fields
           });
         } 
+      });
+    }else{
+      res.status(404).json({
+        error: 'File not found;'
       });
     }
 
